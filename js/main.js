1f49de96dec568e6bfd8b44ad365d903fc18bf5d
Ramiro Amaral <112411299+ramiroamaral10@users.noreply.github.com>
2022-10-26 15:57:10 -0300

    primer entrega

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 //consula de edad
 let edadcliente= parseInt(prompt("Ingrese su edad para entrar al cine"))
 
-if (edadcliente > 18){
+if (edadcliente >= 18){
     console.log("Puedes entrar al cine");
+    //llamo funcion del catalogo de entradas
     catalogoDeEntradas();
 }
 else{
@@ -11,7 +12,7 @@ else{
 }
 
 
-
+//funcion de opciones del catologo de entradas
 function catalogoDeEntradas(){
     alert("Bienvenido a life cinemas")
 
@@ -30,37 +31,51 @@ function catalogoDeEntradas(){
     }
     // el precio de la pelicula es 
     alert("El precio de la entrada es de 200 pesos");
+    //llamo funcion comprar
     comprar()
 
 }
+//funcion comprar sia pagar es true mientras que sea true ejecuta hasta que sea false
 function comprar(){
     let pagar = true;
     while(pagar){
         let preguntacompra = prompt("Estas dispuesto a pagar la entrada? responde con si o no");
         if(preguntacompra == "si"){
-            let cantidad =parseInt(prompt("cuantas desea comprar?"))
+            let cantidad = parseInt(prompt("cuantas desea comprar?"));
             alert ("pago realizado, Disfrute la pelicula");
-            console.log("El costo final de las entradas es de $" + (cantidad*200) + " \npago realizado, Disfrute la pelicula")
+            console.log("El costo final de las entradas es de $" + (cantidad*200) + " \npago realizado, Disfrute la pelicula");
             pagar = false;
-            let opcionesCombos = parseInt(prompt("Elige combo de pop y gaseosa \n1- pop y coca grande \n2 - pop y coca mediano"))
-            ofrescoCombos(opcionesCombos)
+            combos();
 
 
 
-        }
-        else {
+//sino pagar que es igual a faslse dame un alert y que se termine la funcion
+        } else {
             pagar = false
         alert("que tengas un buen dia");
         }
     }
 }
-function ofrescoCombos(opcionesCombos){ 
-    switch (opcionesCombos){
-        case 1:
-            console.log("Elegiste combo mediano")
-            break;
-        case 2:
-            console.log("Elegiste combo grande")
-            
+
+function combos(){
+    let preguntaComboMediano = prompt("desea comprar un combo mediano?");
+    if(preguntaComboMediano == "si"){
+        console.log("buena eleccion");
+        let precioYCantidadComboMediano = parseInt(prompt("el precio del combo es de $250, cuantos desea comprar?"));
+        alert("el precio es de $" + (precioYCantidadComboMediano*250));
+        console.log( "el precio es de $" + (precioYCantidadComboMediano * 250) );
+    } else {
+        alert("okey")
+    }
+    
+    let preguntaComboGrande = prompt("desea comprar un combo grande?");
+    if(preguntaComboGrande == "si"){
+        let precioYCantidadComboGrande = parseInt(prompt("el precio del combo es de $300, cuantos desea comprar?"));
+        alert("el precio es de $" + (precioYCantidadComboGrande*300));
+        console.log( "el precio es de $" + (precioYCantidadComboGrande * 300));
+    } else {
+        alert("bien disfrute la pelicula")
     }
 }
+
+
